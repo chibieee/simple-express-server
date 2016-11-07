45c5b3da83c9137ad97b5392a5bed8ad5fab95a6
chibieee <gregneo21@gmail.com>
2016-11-06 23:37:32 -0500

    added bootstrap styling. layout/about

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ var nodemailer = require("nodemailer");
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
-    res.send('Hello World!');
-})
+    res.render('index', {title: 'Welcome'});
+});
+
+app.get('/about', function(req,res){
+    res.render('about');
+});
 
 app.listen(3000);
 console.log('Server is running on port 3000...');
